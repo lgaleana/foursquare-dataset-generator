4036c920f08a39c61dff702c25cf1b69a32820f7
lgaleana <lsgaleana@gmail.com>
2016-04-02 18:43:10 -0600

    First commit

---
+++ src/main/scala/project/udacity/Scraper.scala
@@ -0,0 +1,32 @@
+package project.udacity
+
+object Scraper {
+  val CLIENT_ID = "QHOC11AI2RYV2J2XHA3CF4C31RULJS4HNQ0JZHL2XI3OKUN3"
+  val CLIENT_SECRET = "SSIEUDJYYZVU2PMPSVX1SRP312TKRC2TGDGEFA5FC0KHIF4D"
+  
+  val categoryIds = Seq(
+      "52e81612bcbc57f1066b7a0d",
+      "56aa371ce4b08b9a8d57356c",
+      "4bf58dd8d48988d117941735",
+      "4bf58dd8d48988d11e941735",
+      "4bf58dd8d48988d118941735",
+      "4bf58dd8d48988d1d8941735",
+      "4bf58dd8d48988d119941735",
+      "4bf58dd8d48988d1d5941735",
+      "4bf58dd8d48988d120941735",
+      "4bf58dd8d48988d11b941735",
+      "4bf58dd8d48988d11c941735",
+      "4bf58dd8d48988d11d941735",
+      "56aa371be4b08b9a8d57354d",
+      "4bf58dd8d48988d122941735",
+      "4bf58dd8d48988d123941735",
+      "50327c8591d4c4b30a586d5d",
+      "4bf58dd8d48988d121941735",
+      "4bf58dd8d48988d11f941735",
+      "4bf58dd8d48988d1d4941735"
+  )
+  
+  def main(args: Array[String]): Unit = {
+    
+  }
+}
